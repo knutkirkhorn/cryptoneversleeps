837e06503e5337f3a2f24e2b3bad679457cdc3ea
Knut Kirkhorn <knutkirk@hotmail.com>
2025-05-05 20:38:32 +0200

    Upgrade to ESLint 9

---
+++ eslint.config.mjs
@@ -0,0 +1,29 @@
+import js from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import eslintPluginUnicorn from 'eslint-plugin-unicorn';
+import {defineConfig} from 'eslint/config';
+import globals from 'globals';
+
+export default defineConfig([
+	js.configs.recommended,
+	eslintPluginUnicorn.configs.recommended,
+	eslintConfigPrettier,
+	{
+		languageOptions: {
+			globals: {...globals.node},
+
+			ecmaVersion: 2025,
+			sourceType: 'module',
+		},
+
+		rules: {
+			'no-console': 'off',
+			'no-plusplus': 'off',
+			'no-await-in-loop': 'off',
+			'no-restricted-syntax': 'off',
+			'no-param-reassign': ['error'],
+			'consistent-return': ['error'],
+			'no-else-return': ['error'],
+		},
+	},
+]);
